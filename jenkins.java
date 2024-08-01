0bb7f01592d1871bd6bdf5dea39863caf8d5dc19
austindk01 <23203018@srcas.ac.in>
2024-08-01 11:53:04 +0530

    Create jenkins.java

---
+++ jenkins.java
@@ -0,0 +1,13 @@
+class Test
+{
+    public static void main(String []args)
+    {
+        System.out.println("My First Java Program.");
+    }
+};class Test
+{
+    public static void main(String []args)
+    {
+        System.out.println("My First Java Program.");
+    }
+};
